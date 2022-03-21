8970486a5a300a8e778072228a2535b2f2744035
Simon <89122143+sikwong2@users.noreply.github.com>
2022-03-20 17:18:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content.includes("fortnite") && message.author != client.user){
+        message.reply("we like fortnite")
+    }
+})
+
+client.login(process.env.TOKEN)
